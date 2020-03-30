21d30f9f4d4c52086b711139dbec642b59a0b677
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2020-03-30 19:43:53 +0100

    had to change the backends.py file to the source code from code institute. They have changed the code from the videos. Dont ask me why. Getting a tutor to look where the error is

--- accounts/backends.py
+++ accounts/backends.py
@@ -1,37 +1,45 @@
-# What is the name that we typically give to a file that contains our custom authentication code? backends.py
-
 from django.contrib.auth.models import User
+from django.db.models import Q
 
-class EmailAuth:
-  """Authenticate a user by an exact match on the email and password"""
 
-  def authenticate(self, username=None, password=None):
-
+class CaseInsensitiveAuth:
     """
-  Get an instance of 'User' based off the email and verify the password
+    Authenticate a of User by using a case-insensitive query to check a
+    combination of the supplied email/username and password.
+    To avoid the risk of having two users with similar usernames,
+    distinguished only by letter case (e.g. 'john' and 'John'), consider
+    updating your User model to save usernames as lower case entries to
+    the database.
+    This will ensure all usernames have unique spellings, and as a result,
+    our case insensitive query will return a single result only.
     """
-    try:
-      user = User.objects.get(email=username)
+    def authenticate(self, username_or_email=None, password=None):
+        """
+        Get an instance of User using the supplied username
+        or email (case insensitive) and verify the password
+        """
+        # Filter all users by searching for a match by username/ email.
+        users = User.objects.filter(Q(username__iexact=username_or_email) |
+                                    Q(email__iexact=username_or_email))
+        if not users:
+            return None
 
-      if user.check_password(password):
-        return user
-      return None
-    except User.DoesNotExist:
-      return None
+        # Then get the first result of the query (which is your user).
+        user = users[0]
+        # If the password is correct, return user object
+        if user.check_password(password):
+            return user
 
-# get_user is a method
-  def get_user(self, user_id):
-    """
-    Used by the django authentication system to retrieve a user instance
-    """
+        return None
 
-    try:
-      user = User.objects.get(pk=user_id)
-
-      if user.is_active:
-        return user
-      return None
-    except User.DoesNotExist:
-      return None
-
-
+    def get_user(self, user_id):
+        """
+        Used by the Django authentication system to retrieve a User instance
+        """
+        try:
+            user = User.objects.get(pk=user_id)
+            if user.is_active:
+                return user
+            return None
+        except User.DoesNotExist:
+            return None
