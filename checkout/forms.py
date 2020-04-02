b19df170b737854cbd393cca7c651c8544222feb
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2020-04-02 20:32:16 +0100

    Using djangos forms capability:Uses a Bootstrap styled form to take information from your user:Pass the data from the form to the models you created in the last unit

---
+++ checkout/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Order
+
+class MakePaymentForm(forms.Form):
+
+  MONTHLY_CHOICES = [(i, i) for i in range(1, 12)]
+  YEAR_CHOICES = [(i, i) for i in range(2017, 2036)]
+
+  credit_card_number = forms.CharField(label='Credit card number', required=False)
+  cvv = forms.CharField(label='Security Code (CW)', required=False)
+  expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
+  expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
+  stripe_id = forms.CharField(widget=forms.HiddenInput)
+
+class OrderForm(forms.ModelForm):
+  class Meta
+      model = Order
+      fields = ('full_name', 'phone_number', 'country', 'postcode', 'town_or_city', 'street_address1', 'streetaddress2', 'county')
